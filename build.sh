02d537d71cbce683f2d9327cbaa0236659ff578f
Daniel Melo <danielmelogpi@gmail.com>
2017-09-05 21:44:59 -0700

    update build and generate scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+rm swagger-codegen*.jar
+wget https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar
+mv swagger-codegen-cli-2.2.1.jar swagger-codegen.jar 
---
+++ generate.sh
@@ -0,0 +1,1 @@
+java -jar swagger-codegen.jar generate -i ./spec/apiv3.swagger.yaml -l swagger -o spec
